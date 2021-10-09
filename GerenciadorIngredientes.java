fda8ab58c18761f3c8da7d9969cd8f439a7f33ec
elisa gonçalves <elisg1711@gmail.com>
2021-10-09 11:39:05 -0300

    primeiro commit

--- GerenciadorIngredientes.java
+++ GerenciadorIngredientes.java
@@ -7,28 +7,32 @@ public class GerenciadorIngredientes {
 
 	static List<Ingrediente> listIng = new ArrayList<>();
 
-	static void comprarEstoque(String nome, double qtd){
+	static void comprarEstoque(Ingrediente ing){
 
-		Ingrediente ing = new Ingrediente();
-		ing.nome = nome;
-		ing.qtdEstoque = qtd;	
+		listIng.add(ing);
+
 	}
 	
 	static void PesquisarIngredientes(String pesquisa) {
+
 		for (int i = 0; i < listIng.size(); i++) {
-			
+
 			if (listIng.get(i).nome.contains(pesquisa)) {
-				System.out.println(listIng.get(i).nome);
-				System.out.println(listIng.get(i).qtdEstoque);
-				System.out.println("");
+				System.out.println("\nINSUMO CADASTRADO COM SUCESSO!");
+				System.out.println("================================================");
+				System.out.println("Nome                	 : " + listIng.get(i).nome);
+				System.out.println("Quantidade no estoque    : " + listIng.get(i).qtdEstoque);
+				System.out.println("Categoria 			     : " + listIng.get(i).categoria);
+				System.out.println("================================================");
 			}
-			
 		}
 	}
-	
+
 	static void baixaEstoque(double removerEstoque) {
-		
+
 		listIng.get(0).removerEstoque(removerEstoque);
-		
+
 	}
+
+
 }
---
+++ Insumo.java
@@ -0,0 +1,13 @@
+package projetos.atividadeEstoque;
+
+public class Insumo {
+	
+	 int codSistema;
+	 String nome;
+	byte categoria;
+	double pCusto;
+	byte medida;
+	double estoqueM;
+	double estoqueA;
+	int sitEstoque;
+}
